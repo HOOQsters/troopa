a7347f59b0aab4dd03f3900810e57febb5464122
Dhi Aurrahman <dio@hooq.tv>
2016-02-19 18:57:39 +0700

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var Linter = require('standard-engine').linter;
+var Linter = require('standard-engine').linter
 
-var opts = require('./options');
+var opts = require('./options')
 
-module.exports = new Linter(opts);
+module.exports = new Linter(opts)
